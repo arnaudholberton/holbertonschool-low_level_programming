1b13da455162795d9e6c38bc675cbf4729c7658e
arnaudholberton <12052@holbertonstudents.com>
2025-10-20 13:03:31 +0000

    6

---
+++ hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - Function
+*Affiche les different type de fichier
+*Return: 0 ci code valide
+*/
+int main(void)
+{
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+return(0);
+}
