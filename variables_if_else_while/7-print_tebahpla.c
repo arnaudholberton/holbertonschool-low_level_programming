0ea6ccc6536906541837d61d580653f24f0f9b82
arnaudholberton <12052@holbertonstudents.com>
2025-10-21 13:34:50 +0000

    7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - afficher nombre 0 9
+*
+*Return: renvoi 0
+*/
+int main(void)
+{
+int a = 'z';
+while (a >= 'a')
+{
+putchar(a);
+a--;
+}
+putchar('\n');
+return (0);
+}
