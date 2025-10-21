040ce95a3385c43d8c96f19e3791c570104b36e2
arnaudholberton <12052@holbertonstudents.com>
2025-10-21 09:53:08 +0000

    1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ printf("Last digit of %d  is %d and is 0\n", n, nbr);
 }
 else
 {
-printf("Last digit of %d  is %d and is less than 6 and not 0\n", n, nbr);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, nbr);
 }
 return (0);
 }
