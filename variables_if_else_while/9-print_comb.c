efbbae7175d33c66e1ca731a0d2d919fff6298cb
arnaudholberton <12052@holbertonstudents.com>
2025-10-21 14:46:19 +0000

    10

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - afficher nombre 0 9
+*
+*Return: renvoi 0
+*/
+int main(void)
+{
+int a = 0;
+while (a < 10)
+	{
+	putchar(a + '0');
+	a++;
+	if (a != 10){
+		putchar(',');
+	}
+putchar(' ');  
+	}
+putchar('\n');
+return (0);
+}
