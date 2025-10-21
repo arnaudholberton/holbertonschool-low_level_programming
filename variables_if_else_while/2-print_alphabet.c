4714839ea51cb2fc3b5f8dc6d3b3144d4d053ee8
arnaudholberton <12052@holbertonstudents.com>
2025-10-21 10:16:47 +0000

    2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -5,9 +5,11 @@
 *main - liste les lettres
 *Return: renvoi 0 ci code ok
 */
+
 int main(void)
 {
-for (int c = 'a'; c <= 'z'; c++)
+int c = a;
+for (c = 'a'; c <= 'z'; c++)
 {
 putchar(c);
 putchar('\n');
