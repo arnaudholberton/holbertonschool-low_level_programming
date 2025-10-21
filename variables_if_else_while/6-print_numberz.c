184a06850631154fb5148753cab739d2775cd967
arnaudholberton <12052@holbertonstudents.com>
2025-10-21 13:16:23 +0000

    6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - afficher nombre 0 9
+*
+*Return: renvoi 0
+*/
+int main(void)
+{
+int a = 0;
+while (a != 10)
+	{
+	putchar("%d", a);
+	a++;
+	}
+putchar("\n");
+return (0);
+}
