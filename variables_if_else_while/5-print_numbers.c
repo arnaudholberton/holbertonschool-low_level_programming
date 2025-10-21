f613365b7f88ce7636c1540f193edf4eaa126d35
arnaudholberton <12052@holbertonstudents.com>
2025-10-21 13:03:54 +0000

    5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - afficher nombre 0 9
+*
+*Return: renvoi 0
+*/
+int main(void)
+{
+int a = 0;
+while (a != 10)
+	{
+	printf("%d", a);
+	a++;
+	}
+printf("\n");
+return (0);
+}
