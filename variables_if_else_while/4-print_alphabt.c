2a4dbb3990b7c86a12db1666c44a936ce201b11e
arnaudholberton <12052@holbertonstudents.com>
2025-10-21 10:47:38 +0000

    4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - affiche des lettre
+*Return: renvoi 0
+*
+*/
+int main(void)
+{
+char a;
+for (a = 'a'; a = 'z'; a++)
+{
+if (a != 'q' || a != 'e')
+{
+putchar(a);
+}
+}
+return (0);
+}
