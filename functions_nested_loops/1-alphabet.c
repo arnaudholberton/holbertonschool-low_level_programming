49a13e704e15a7835c3496c159cb41ae6d2718f9
arnaudholberton <12052@holbertonstudents.com>
2025-10-22 16:41:18 +0200

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_alphabet - afichage lettre
+*_putchar; imprime caractere
+*
+*/
+void print_alphabet(void)
+{
+char a = 'a';
+while (a <= 'z')
+{
+_putchar(a);
+a++;
+}
+}
---
+++ functions_nested_loops/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - point d'entrée du programme
+ * Return: Always 0
+ */
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
