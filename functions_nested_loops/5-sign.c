159237272dfd7f2c92b1d3fc3fce42f8c4d1b0e9
arnaudholberton <12052@holbertonstudents.com>
2025-10-23 11:24:26 +0200

    5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+*print_sign - function compare miniscule
+*@n : lettre a comparer
+*
+*Return: sinon 0
+*
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+if (n < 0)
+{
+_puthcar('-');
+return (-1);
+}
+}
