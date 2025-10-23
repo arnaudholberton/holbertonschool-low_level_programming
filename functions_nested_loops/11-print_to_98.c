f47028b75ad19ac5e765b825e0cb19351cbbe0ba
arnaudholberton <12052@holbertonstudents.com>
2025-10-23 18:50:46 +0200

    11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*print_to_98 - imprime une suite de nombre
+*
+*/
+void print_to_98(int a)
+{
+if (a <= 98)
+	{
+		for (; a <= 98 ; a++)
+			{
+			printf("%d", a);
+if (a != 98)
+{
+					printf(", ");
+}
+			}
+			printf("\n");
+	}
+	else if (a > 98)
+	{
+		for (; a >= 98; a--)
+		{
+		printf("%d", a);
+				if (a != 98)
+{
+					printf(", ");
+}
+		}
+		printf("\n");
+	}
+else
+	{
+		printf("98");
+	}
+}
