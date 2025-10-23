99dd0ab7d1b836b45945ead44d3ccd80e21ec8ab
arnaudholberton <12052@holbertonstudents.com>
2025-10-23 09:54:25 +0200

    2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - afichage lettre
+*Return: renvoi 0
+*
+*/
+int print_alphabet_x10(void)
+{
+char a = 'a';
+char b = 0;
+while (b <= 10)
+{
+while (a <= 'z')
+{
+_putchar(a);
+a++;
+}
+b++;
+_putchar('\n');
+}
+return (0);
+}
