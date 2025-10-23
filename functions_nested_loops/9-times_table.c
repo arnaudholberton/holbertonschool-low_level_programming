e3838514cfb9ad83f6d56d8ef51711bba930efc2
arnaudholberton <12052@holbertonstudents.com>
2025-10-23 16:56:51 +0200

    9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+*times_table - affiche toute les heures
+*jusqua 23h
+*
+*/
+void times_table(void)
+{
+int ligne;
+int colone;
+int nbr;
+for (ligne = 0, ligne <= 9, ligne++)
+{
+putchar('0');
+for (colone = 1, colone == 9, colone++)
+{
+nbre = colone * ligne;
+_putchar(',');
+_putchar(' ');
+if (nbr <= 9)
+{
+_putchar(' ');
+_putchar(nbr + '0');
+}
+else
+{
+_putchar(nbr / 10 + '0');
+_putchar(nbr % 10 + '0');
+}
+}
+_putchar(('\n'));
+}
+}
