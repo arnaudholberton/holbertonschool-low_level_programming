ab96bb7d39e871d82cddf760e7eef42b1955bf2e
arnaudholberton <12052@holbertonstudents.com>
2025-10-25 16:07:41 +0200

    100

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -3,25 +3,24 @@
 *main - afficher nombre 0 9
 *Return: renvoi 0
 */
-int main(void
+int main(void)
 {
-	int a[], b[];
-while (condition)
+int b = 0;
+int a = 0;
+for (b = 0; a <= 9; a++)
 {
-	/* code  01 02 03 ... 09 verif ci 0 . 1 ou 1 . 0. deja ok*/
-
+for (b = 0; b <= 9 ; b++)
+{
+if (a < b)
+{
+putchar(a + '0');
+putchar(b + '0');
+if (a < 8)
+{
+putchar(',');
+putchar(' ');
 }
-ci egal a moi meme non if a==b
-ci 
-})a +b = 1 c+1 >> ci c == 1 non pour 10 
-
-int 1
-int 2
-int 3
-impriime 0 1 
-la conbinaison de a +b = v1 =true
- 
-
-
-
-1 0 ci v1 == true rien 
+}
+}
+}
+}
