4eb560ac77a4ac0ac2b3ea0b85663a383612b620
arnaudholberton <12052@holbertonstudents.com>
2025-10-27 10:27:52 +0100

    1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,12 @@
 #include "main.h"
-/*
+/**
 *_isdigit - compare chiffre
-*
-*
+*@c : a comparer
+*Return: ci chiffre 0
 */
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= 0 || c <= 9)
 	return (1);
 	else
 	return (0);
