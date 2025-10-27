81d05f19763ecf54e29b3ec71efe187578de66ec
arnaudholberton <12052@holbertonstudents.com>
2025-10-27 09:47:23 +0100

    0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,14 @@
 #include "main.h"
-/*
-*_isupper - verif ci majusculte
-*Return - 1 ci maj sinon 0
+/**
+*_isupper - function compare miniscule
+*@c : lettre a comparer
+*
+*Return: sinon 0
+*
 */
 int _isupper(int c)
 {
-	if (c == 'A')
+	if (c >= 'A' && c <= 'Z')
 	return (1);
 	else
 	return (0);
