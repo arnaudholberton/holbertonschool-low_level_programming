83afb77119795c60270284168ed7b9806af0ecb1
arnaudholberton <12052@holbertonstudents.com>
2025-10-27 13:18:02 +0100

    6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line - prints 10 times the numbers, from 0 to 14
+ *
+ *
+ */
+void print_line(int n)
+{
+	int c;
+for(c = 0; c < n; c++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
