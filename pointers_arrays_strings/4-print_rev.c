d6119cc49f7ba2f2bc30335d7a3bbc9a4fb1e686
arnaudholberton <12052@holbertonstudents.com>
2025-10-28 17:08:49 +0100

    4

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,13 @@ void print_rev(char *s)
 {
 int b;
 int c = 0;
-for ( b = 0; s[b] != '\0'; b++)
+for (b = 0; s[b] != '\0'; b++)
 {
 c++;
 }
 for ( ; b >= 0 ; b--)
 {
-if(s[b] != '\0')
+if (s[b] != '\0')
 _putchar(s[b]);
 }
 _putchar('\n');
