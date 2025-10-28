82f79e493235f24a6846315b6aab8434aa1cb013
arnaudholberton <12052@holbertonstudents.com>
2025-10-28 10:57:46 +0100

    10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -7,25 +7,25 @@ print_triangle - Function for print # triangle
 
 void print_triangle(int size)
 {
-    int i, j, k;
+		int i, j, k;
 
-    if (size <= 0)
-    {
-        _putchar('\n');
-    }
-    else
-    {
-        for (i = 1; i <= size ; i++)
-        {
-            for (j = 1; j <= size - i; j++)
-            {
-                _putchar(' ');
-            }
-            for (k = 1; k <= i; k++)
-            {
-                _putchar('#');
-            }
-        _putchar('\n');
-        }
-    }
+		if (size <= 0)
+		{
+				_putchar('\n');
+		}
+		else
+		{
+				for (i = 1; i <= size ; i++)
+				{
+						for (j = 1; j <= size - i; j++)
+						{
+								_putchar(' ');
+						}
+						for (k = 1; k <= i; k++)
+						{
+								_putchar('#');
+						}
+				_putchar('\n');
+				}
+		}
 }
