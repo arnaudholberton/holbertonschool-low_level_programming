72afe7f7d4b4cb7420aabe8a23cc93d40c767a5a
arnaudholberton <12052@holbertonstudents.com>
2025-10-28 17:36:27 +0100

    5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+int b;
+int c = 0;
+for (b = 0; s[b] != '\0'; b++)
+{
+c++;
+}
+for ( ; b >= 0 ; b--)
+{
+if (s[b] != '\0')
+_putchar(s[b]);
+}
+_putchar('\n');
+}
