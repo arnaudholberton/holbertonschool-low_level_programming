43815a7949c4b63090235a46c5129efd812bb88c
arnaudholberton <12052@holbertonstudents.com>
2025-10-29 16:36:53 +0100

    10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,12 +1,5 @@
 #include "main.h"
 /**
- * _atoi - converts a string to an integer
- * @s: pointer to the string to be converted
- * Return: the converted integer value
- */
-#include "main.h"
-
-/**
  * _atoi - convertit une chaîne en entier
  * @s: chaîne à convertir
  *
@@ -14,30 +7,24 @@
  */
 int _atoi(char *s)
 {
-    int i = 0;
-    int sign = 1;
-    unsigned int num = 0;
-    int found_digit = 0;
-
-    while (s[i] != '\0')
-    {
-        if (s[i] == '-')
-            sign *= -1;
-
-        else if (s[i] >= '0' && s[i] <= '9')
-        {
-            found_digit = 1;
-            num = num * 10 + (s[i] - '0');
-        }
-
-        else if (found_digit)
-            break;
-
-        i++;
-    }
-
-    if (!found_digit)
-        return 0;
-
-    return (sign * num);
+int i = 0;
+int sign = 1;
+unsigned int num = 0;
+int found_digit = 0;
+while (s[i] != '\0')
+{
+if (s[i] == '-')
+sign *= -1;
+else if (s[i] >= '0' && s[i] <= '9')
+{
+found_digit = 1;
+num = num * 10 + (s[i] - '0');
+}
+else if (found_digit)
+break;
+i++;
+}
+if (!found_digit)
+return 0;
+return (sign * num);
 }
