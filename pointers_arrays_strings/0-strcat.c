3fdd16274523cb1c3fcee3dcbc6d1b74c501c089
arnaudholberton <12052@holbertonstudents.com>
2025-10-29 18:33:52 +0100

    0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,3 +1,11 @@
+#include "main.h"
+/**
+ * _strcat - concatenate two strings
+ * @dest: pointer to destination string
+ * @src: pointer to source string
+ * Return: pointer to the resulting string
+ */
+
 char *_strcat(char *dest, char *src)
 {
 	int i = 0;
@@ -6,7 +14,7 @@ while (dest[i] != '\0')
 {
 	i++;
 }
-for ( ; src[ii] != '\0'; ii++)
+for (ii = 0; src[ii] != '\0'; ii++)
 {
 dest[i] = src[ii];
 i++;
