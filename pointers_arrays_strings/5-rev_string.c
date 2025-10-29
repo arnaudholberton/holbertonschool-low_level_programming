ada6256f3dfd099d24b82ee8b6ff2e3cf2c53c5a
arnaudholberton <12052@holbertonstudents.com>
2025-10-29 09:09:28 +0100

    5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -9,14 +9,14 @@ int b;
 int c = 0;
 for (b = 0; s[b] != '\0'; b++)
 {
-	_putchar(s[b]);
+
 c++;
 }
-_putchar('\n');
+
 for ( ; b <= 0 ; b--)
 {
 if (s[b] != '\0')
-_putchar(s[b]);
+
 }
-_putchar('\n');
+
 }
