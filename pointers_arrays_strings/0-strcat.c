511ff2133e7a83e48471cbc63faffac999f5e831
arnaudholberton <12052@holbertonstudents.com>
2025-10-29 18:24:06 +0100

    0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,8 +1,15 @@
 char *_strcat(char *dest, char *src)
 {
-	int t;
-t = *src;
-*src = *dest;
-*dest = t;
+	int i = 0;
+	int ii = 0;
+while (dest[i] != '\0')
+{
+	i++;
+}
+for ( ; src[ii] != '\0'; ii++)
+{
+dest[i] = src[ii];
+i++;
+}
 	return (dest);
 }
