3a07ef97b1f67f40187e1b909c8deb536e8f0547
arnaudholberton <12052@holbertonstudents.com>
2025-10-31 10:20:09 +0100

    4

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -3,11 +3,11 @@
  * reverse_array - reverses the content of an array of integers
  * @a: pointer to the first element of the array
  * @n: number of elements of the array
- */	
+ */
 void reverse_array(int *a, int n)
 {
 int *debut = a;
-int *fin = a + n -1;
+int *fin = a + n - 1;
 int temp;
 while (debut < fin)
 {
