11e6ba2331a3ed15fdb3f516f13ad07d875afebe
arnaudholberton <12052@holbertonstudents.com>
2025-10-31 11:29:23 +0100

    5

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * string_toupper - changes all lowercase letters of a string to uppercase
- * @a: pointer to the string to be modified
+ * @b: pointer to the string to be modified
  * Return: pointer to the modified string
  */
 char *string_toupper(char *b)
