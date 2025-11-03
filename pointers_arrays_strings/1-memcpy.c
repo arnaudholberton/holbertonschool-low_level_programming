db12ce8236dd61710a2edd8481c4f2283cc4d317
arnaudholberton <12052@holbertonstudents.com>
2025-11-03 10:46:25 +0100

    1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,3 +1,12 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory area
+ * @dest: pointer to the destination memory area
+ * @src: pointer to the source memory area
+ * @n: number of bytes to copy
+ * Return: pointer to the destination memory area dest
+ */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	char *a = dest;
