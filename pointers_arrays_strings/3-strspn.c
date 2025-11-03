a457d14d3a32a58b1ecdd4a30b9d5cd0f9b1a192
arnaudholberton <12052@holbertonstudents.com>
2025-11-03 13:02:20 +0100

    3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,4 +1,22 @@
+	#include "main.h"
 unsigned int _strspn(char *s, char *accept)
 {
-	
+	unsigned int n = 0;
+
+	while (*s)
+	{
+		char *a = accept;
+
+		while (*a && *a != *s)
+		{
+			a++;
+		}
+		if (!*a)
+		{
+			break;
+		}
+		n++;
+		s++;
+	}
+	return (n);
 }
