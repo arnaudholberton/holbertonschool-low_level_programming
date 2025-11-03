3c1790366fd6ca9e8d2f5fc57460ab9a6166ec4e
arnaudholberton <12052@holbertonstudents.com>
2025-11-03 10:47:51 +0100

    1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -9,11 +9,11 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *a = dest;
-	unsigned int i;
-	for (i = 0; i < n; i++)
-	{
+char *a = dest;
+unsigned int i;
+for (i = 0; i < n; i++)
+{
 dest[i] = src[i];
-	}
-	return (a);
+}
+return (a);
 }
