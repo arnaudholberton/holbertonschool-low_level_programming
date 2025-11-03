d5752d2644c479136062d94e9eff530603f38522
arnaudholberton <12052@holbertonstudents.com>
2025-11-03 16:32:29 +0100

    7

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -7,14 +7,14 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i;
-	int ii;
-	for (i = 0; i < 8; i++) 
-	{
-		for (ii = 0; ii < 8; ii++) 
-		{
-			_putchar(a[i][ii]);
-		}
-		_putchar('\n');
-	}
+int i;
+int ii;
+for (i = 0; i < 8; i++)
+{
+for (ii = 0; ii < 8; ii++)
+{
+_putchar(a[i][ii]);
+}
+_putchar('\n');
+}
 }
