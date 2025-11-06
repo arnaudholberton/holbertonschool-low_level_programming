6536fa52205c1182177a4adc526a49e456ad95e8
arnaudholberton <12052@holbertonstudents.com>
2025-11-06 13:11:59 +0100

    4

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -9,22 +9,22 @@
  */
 int main(int argc, char **argv)
 {
-	int i, ii, somme = 0;
+int i, ii, somme = 0;
 if (argc == 1)
-	{
-		printf("0\n");
-		return (0);
-	}
-	for (i = 1; i < argc; i++)
-	{
-		for (ii = 0; argv[i][ii] != '\0'; ii++)
-		if (argv[i][ii] < '0' || argv[i][ii] > '9')
-		{
-			printf("Error\n");
-			return (1);
-		}
-		somme += atoi(argv[i]);
-	}
-	printf("%d\n", somme);
-	return (0);
+{
+printf("0\n");
+return (0);
+}
+for (i = 1; i < argc; i++)
+{
+for (ii = 0; argv[i][ii] != '\0'; ii++)
+if (argv[i][ii] < '0' || argv[i][ii] > '9')
+{
+printf("Error\n");
+return (1);
+}
+somme += atoi(argv[i]);
+}
+printf("%d\n", somme);
+return (0);
 }
