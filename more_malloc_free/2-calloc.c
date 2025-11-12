e30191af25423b7c48896bc952fdec4024f35d74
arnaudholberton <12052@holbertonstudents.com>
2025-11-12 15:07:03 +0100

    2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * _calloc - allocates memory for an array, using malloc
  * @nmemb: number of elements
@@ -20,5 +21,6 @@ if (nmemb == 0 || size == 0)
 		{
 			return (NULL);
 		}
+	memset(a, 0, nmemb * size);
 	return (a);
 }
