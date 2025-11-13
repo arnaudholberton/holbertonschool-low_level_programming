e74be1a2ac6d145fd5ebd53ab73570475e416283
arnaudholberton <12052@holbertonstudents.com>
2025-11-13 16:13:33 +0100

    0

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdlib.h>
-struct dog { 
+/**
+ * struct dog - A structure to represent a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
