7a5f7809aef2692860427024d031b3489a84d0c6
arnaudholberton <12052@holbertonstudents.com>
2025-11-14 15:12:05 +0100

    5

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - frees memory allocated for a dog_t structure
+ * @d: pointer to the dog_t structure to be freed
+ *
+ * Return: Nothing.
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	return;
+	free(d);
+}
