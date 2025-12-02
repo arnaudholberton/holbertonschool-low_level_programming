1c5a4e9f28d335c2bf6869f2c8da92533de16e2f
arnaudholberton <12052@holbertonstudents.com>
2025-12-02 15:29:17 +0100

    Correction de la documentation de la fonction print_dlistint pour refléter correctement son comportement

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -2,14 +2,14 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-size_t print_dlistint(const dlistint_t *h)
-{
 /**
  * print_list - prints all the elements of a list_t list
  * @h: pointer to the head of the list
  *
  * Return: number of nodes in the list
  */
+size_t print_dlistint(const dlistint_t *h)
+{
 size_t count = 0;
 if (h == NULL)
 {
@@ -17,7 +17,6 @@ if (h == NULL)
 }
 while (h != NULL)
 {
-	
 	printf("%d\n", h->n);
 	count++;
 	h = h->next;
