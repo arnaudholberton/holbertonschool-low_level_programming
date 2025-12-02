1154157c98c3b716dc5711f5a62a7bfcf75eaafe
arnaudholberton <12052@holbertonstudents.com>
2025-12-02 15:28:27 +0100

    Ajout des fichiers principaux pour la gestion des listes doublement chaînées et implémentation des fonctions de base

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+size_t print_dlistint(const dlistint_t *h)
+{
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: number of nodes in the list
+ */
+size_t count = 0;
+if (h == NULL)
+{
+	return (0);
+}
+while (h != NULL)
+{
+	
+	printf("%d\n", h->n);
+	count++;
+	h = h->next;
+}
+return (count);
+}
